771a6213d02e34b1185bc9761a91d80a3e57409a
Abel Wendmu <93484932+legend123213@users.noreply.github.com>
2024-07-08 21:09:12 +0300

    Created 'Convert the Temperature' by A2SV Hub

---
+++ 08-Jul-2024/Convert the Temperature 177837.py
@@ -0,0 +1,8 @@
+# Problem: Convert the Temperature - https://leetcode.com/problems/convert-the-temperature/
+
+class Solution:
+    def convertTemperature(self, celsius: float) -> List[float]:
+        kelvin = round(celsius + 273.15,5)
+        fahrenheit = round(celsius * 1.80 + 32.00,5)
+        return [kelvin,fahrenheit]
+        
